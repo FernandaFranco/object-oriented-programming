b1c28e3f35275c24699280941885e26a48741778
Fernanda Franco <francodias.fernanda@gmail.com>
2017-02-01 18:31:34 -0500

    bonus features sufficiently completed

--- lesson_5/bonus_feat_oo_tic_tac_toe.rb
+++ lesson_5/bonus_feat_oo_tic_tac_toe.rb
@@ -9,13 +9,86 @@ class Array
   end
 end
 
+class Board
+  def initialize
+    @squares = {}
+    reset
+  end
+
+  # rubocop:disable Metrics/AbcSize
+  def draw
+    puts "     |     |     "
+    puts "  #{@squares[1]}  |  #{@squares[2]}  |  #{@squares[3]}"
+    puts "     |     |     "
+    puts "-----+-----+-----"
+    puts "     |     |     "
+    puts "  #{@squares[4]}  |  #{@squares[5]}  |  #{@squares[6]}"
+    puts "     |     |     "
+    puts "-----+-----+-----"
+    puts "     |     |     "
+    puts "  #{@squares[7]}  |  #{@squares[8]}  |  #{@squares[9]}"
+    puts "     |     |     "
+  end
+  # rubocop:enable Metrics/AbcSize
+
+  def []=(key, marker)
+    @squares[key].marker = marker
+  end
+
+  def unmarked_keys
+    @squares.keys.select { |key| @squares[key].unmarked? }
+  end
+
+  def marked_by_human_keys
+    @squares.keys.select { |key| @squares[key].marked_by_human? }
+  end
+
+  def marked_by_computer_keys
+    @squares.keys.select { |key| @squares[key].marked_by_computer? }
+  end
+
+  def full?
+    unmarked_keys.empty?
+  end
+
+  def reset
+    (1..9).each { |key| @squares[key] = Square.new }
+  end
+end
+
+class Square
+  INITIAL_MARKER = ' '.freeze
+
+  attr_accessor :marker
+
+  def initialize
+    @marker = INITIAL_MARKER
+  end
+
+  def unmarked?
+    marker == INITIAL_MARKER
+  end
+
+  def marked_by_human?
+    marker == Human::HUMAN_MARKER
+  end
+
+  def marked_by_computer?
+    marker == Computer::COMPUTER_MARKER
+  end
+
+  def to_s
+    marker
+  end
+end
+
 class Player
   WINING_SETS = [[1, 2, 3], [4, 5, 6], [7, 8, 9],
                  [1, 4, 7], [2, 5, 8], [3, 6, 9],
                  [1, 5, 9], [3, 5, 7]].freeze
 
   attr_reader :marker
-  attr_accessor :marked_keys, :score
+  attr_accessor :marked_keys, :score, :name
 
   def initialize
     reset
@@ -68,124 +141,59 @@ class Human < Player
 end
 
 class Computer < Player
+  STANDARD_FIRST_KEY = 5
   COMPUTER_MARKER = "O".freeze
 
   def initialize
     super
     @marker = COMPUTER_MARKER
+    set_name
   end
 
-  def try_offensive_move(set, board)
-    if marked_keys.count { |k| set.include?(k) } == 2
-      set.select { |k| board.unmarked_keys.include?(k) }.first
-    end
-  end
-
-  def try_defensive_move(set, board)
-    if board.marked_by_human_keys.count { |k| set.include?(k) } == 2
-      set.select { |k| board.unmarked_keys.include?(k) }.first
-    end
+  def set_name
+    self.name = %w(Robert Viviane Alyssa Maddie Boyle).sample
   end
 
   def move(board)
     key = nil
 
     WINING_SETS.each do |set|
-      key ||=  try_offensive_move(set, board)
+      key ||= offensive_move_key(set, board) # still nil if no offense move
     end
 
     WINING_SETS.each do |set|
-      key ||= try_defensive_move(set, board)
+      key ||= defensive_move_key(set, board) # still nil if no defense move
     end
 
-    if board.unmarked_keys.include?(5)
-      key ||= 5
-    else
-      key ||= board.unmarked_keys.sample
-    end
+    key ||= if board.unmarked_keys.include?(STANDARD_FIRST_KEY)
+              STANDARD_FIRST_KEY
+            else
+              board.unmarked_keys.sample
+            end
 
     board[key] = marker
 
     marked_keys << key
   end
-end
 
-class Board
-  def initialize
-    @squares = {}
-    reset
+  private
+
+  def offensive_move_key(set, board)
+    if marked_keys.count { |k| set.include?(k) } == 2
+      set.select { |k| board.unmarked_keys.include?(k) }.first
+    end
   end
 
-  # rubocop:disable Metrics/AbcSize
-  def draw
-    puts "     |     |     "
-    puts "  #{@squares[1]}  |  #{@squares[2]}  |  #{@squares[3]}"
-    puts "     |     |     "
-    puts "-----+-----+-----"
-    puts "     |     |     "
-    puts "  #{@squares[4]}  |  #{@squares[5]}  |  #{@squares[6]}"
-    puts "     |     |     "
-    puts "-----+-----+-----"
-    puts "     |     |     "
-    puts "  #{@squares[7]}  |  #{@squares[8]}  |  #{@squares[9]}"
-    puts "     |     |     "
-  end
-  # rubocop:enable Metrics/AbcSize
-
-  def []=(key, marker)
-    @squares[key].marker = marker
-  end
-
-  def unmarked_keys
-    @squares.keys.select { |key| @squares[key].unmarked? }
-  end
-
-  def marked_by_human_keys
-    @squares.keys.select { |key| @squares[key].marked_by_human? }
-  end
-
-  def marked_by_computer_keys
-    @squares.keys.select { |key| @squares[key].marker_by_computer? }
-  end
-
-  def full?
-    unmarked_keys.empty?
-  end
-
-  def reset
-    (1..9).each { |key| @squares[key] = Square.new }
-  end
-end
-
-class Square
-  INITIAL_MARKER = ' '.freeze
-
-  attr_accessor :marker
-
-  def initialize
-    @marker = INITIAL_MARKER
-  end
-
-  def unmarked?
-    marker == INITIAL_MARKER
-  end
-
-  def marked_by_human?
-    marker == Human::HUMAN_MARKER
-  end
-
-  def marker_by_computer?
-    marker == Computer::COMPUTER_MARKER
-  end
-
-  def to_s
-    marker
+  def defensive_move_key(set, board)
+    if board.marked_by_human_keys.count { |k| set.include?(k) } == 2
+      set.select { |k| board.unmarked_keys.include?(k) }.first
+    end
   end
 end
 
 class TTTGame
   FIRST_TO_MOVE = Human::HUMAN_MARKER
-  WINS_TO_END_GAME = 3
+  WINS_TO_END_GAME = 5
 
   attr_reader :board, :human, :computer
   attr_accessor :turn
@@ -199,6 +207,7 @@ class TTTGame
 
   def play
     display_welcome_message
+
     loop do
       display_board
 
@@ -209,7 +218,6 @@ class TTTGame
       end
 
       display_result
-      update_and_display_score
       break if end_game? || !play_again?
       reset
       display_play_again_message
@@ -225,17 +233,34 @@ class TTTGame
     system('clc')
   end
 
+  def set_human_name
+    answer = ''
+    loop do
+      puts "What's your name?"
+      answer = gets.chomp.capitalize
+      break unless answer.empty?
+      puts "Sorry, can't be empty."
+    end
+    clear
+    human.name = answer
+  end
+
   def display_welcome_message
     clear
     puts "Welcome to Tic Tac Toe!"
+
+    set_human_name
+    puts "Nice to meet you, #{human.name}! " \
+         "Your opponent's name is #{computer.name}."
   end
 
   def display_goodbye_message
-    puts "Thanks for playing Tic Tac Toe. Bye-bye!"
+    puts "Thanks for playing Tic Tac Toe, #{human.name}. Bye-bye!"
   end
 
   def display_board
-    puts "You are '#{human.marker}'. Computer is '#{computer.marker}'."
+    puts "#{human.name} is '#{human.marker}'"
+    puts "#{computer.name} is '#{computer.marker}'."
     puts ""
     board.draw
     puts ""
@@ -252,9 +277,7 @@ class TTTGame
     human.won? || computer.won?
   end
 
-  def display_result
-    clear_screen_and_display_board
-
+  def display_winner
     if human.won?
       puts "You win!"
     elsif computer.won?
@@ -264,19 +287,28 @@ class TTTGame
     end
   end
 
-  def update_and_display_score
+  def update_score
     if human.won?
       human.score += 1
     elsif computer.won?
       computer.score += 1
     end
+  end
 
+  def display_score
     puts "Human: #{human.score} X Computer: #{computer.score}"
     if human.score == WINS_TO_END_GAME
       puts "Congratulations, you won #{WINS_TO_END_GAME} games!"
     elsif computer.score == WINS_TO_END_GAME
       puts "Computer won #{WINS_TO_END_GAME} games. Better luck next time!"
     end
+  end
+
+  def display_result
+    clear_screen_and_display_board
+    display_winner
+    update_score
+    display_score
   end
 
   def end_game?
