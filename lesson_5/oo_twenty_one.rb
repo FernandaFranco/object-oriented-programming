c9fcb5a20a2fcecc4898c55b2c249b43b98edac4
Fernanda Franco <francodias.fernanda@gmail.com>
2017-02-01 18:59:19 -0500

    started implementing 00 twenty one

---
+++ lesson_5/oo_twenty_one.rb
@@ -0,0 +1,11 @@
+description: twenty-one is a game with a dealer and a player where the goal is
+to get closer to 21 with the sum of the values of the cards in your hand without
+busting. Every player starts with 2 cards. A player can hit until he makes 21
+or stay, giving the turn to the dealer. The dealer has to keep hitting until he
+makes 17. Whoever gets closer to 21 without busting is the winner.
+
+dealer
+player
+hit
+stay
+bust
